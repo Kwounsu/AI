b26c4842c0516a8893ffbc3e6f6d3c2a62f58bac
Kwounsu Lee <sb00lgs@gmail.com>
2021-04-29 14:33:46 +0900

    add IRQ code

--- 사분위수.py
+++ 사분위수.py
@@ -15,6 +15,8 @@ B_Q2 = store_b.quantile(0.50)  # 2사분위수: 22.0 (중앙값)
 B_Q3 = store_b.quantile(0.75)  # 3사분위수: 34.0
 B_Q4 = store_b.quantile(1)     # 4사분위수: 500.0
 
+
+# Boxplot
 plt.boxplot(store_a)
 plt.grid()
 plt.show()
@@ -22,3 +24,17 @@ plt.show()
 plt.boxplot(store_b)
 plt.grid()
 plt.show()
+
+
+# IRQ
+box_nums = pd.Series([16, 21, 22, 23, 24, 25, 30])
+
+Q1 = box_nums.quantile(0.25)
+Q2 = box_nums.quantile(0.50)
+Q3 = box_nums.quantile(0.75)
+Q4 = box_nums.quantile(1)
+
+IRQ = Q3 - Q1             # 3.0
+STEP1 = IRQ * 1.5         # 4.5
+lower_fence = Q1 - STEP1  # 17.0 (이 값보다 작으면 이상치)
+upper_fence = Q3 + STEP1  # 29.0 (이 값보다 높으면 이상치)
